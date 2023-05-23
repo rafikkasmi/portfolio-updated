b89d9198ea196eb1a95029c84a96914dff140e63
rafikkasmi <rafikhdey@gmail.com>
2023-05-23 08:51:08 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <HomeLanding/>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $gsap } = useNuxtApp()
+
+onMounted(() => {
+  $gsap.to('h1', { rotation: 3, x: 100, duration: 1 })
+})
+</script>
---
+++ components/Home/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="w-full ">
+
+    <div class="text flex flex-col">
+        <h1 class="text-5xl">
+            I build websites that meet your business' needs
+        </h1>
+        <button class="w-44 h-14 rounded-full bg-red-400 text-white">
+            Get in touch
+        </button>
+    </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'Landing',
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
